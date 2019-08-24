a24431200b37eb7dc353c91bb1114e43fda4ea3a
pos liberado <pospos@labcomp.usc>
2019-08-24 09:30:20 -0300

    alteração

--- src/app/dados/dados.page.ts
+++ src/app/dados/dados.page.ts
@@ -9,7 +9,7 @@ export class DadosPage implements OnInit {
   private user: object;
 
   constructor() {
-    this.user = {name: "Fabiana", idade: "24", email: "fabianarminutti@gmail.com", telefone: "(14) 99999-9999"}
+    this.user = {name: "Cassia", idade: "33", email: "caslencoispta@hotmail.com", telefone: "(14) 99999-9999"}
   }
 
   ngOnInit() {
